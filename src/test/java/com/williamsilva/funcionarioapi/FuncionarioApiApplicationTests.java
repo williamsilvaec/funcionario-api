94848cfee6514e01708dd9e53893cad465b17068
william.silva <william@viasoft.com.br>
2021-05-28 15:10:17 -0300

    init

---
+++ src/test/java/com/williamsilva/funcionarioapi/FuncionarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamsilva.funcionarioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionarioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

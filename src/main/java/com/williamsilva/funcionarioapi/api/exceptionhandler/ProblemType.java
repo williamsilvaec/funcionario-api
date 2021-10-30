d9ae023195bb5296676e3a013a4f766f9383f91e
william.silva <william@viasoft.com.br>
2021-10-30 10:27:51 -0300

    Ajustes nas mensagens de erros

--- src/main/java/com/williamsilva/funcionarioapi/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/williamsilva/funcionarioapi/api/exceptionhandler/ProblemType.java
@@ -2,12 +2,12 @@ package com.williamsilva.funcionarioapi.api.exceptionhandler;
 
 public enum ProblemType {
 
-    RECURSO_NAO_ENCONTRADO("Recurso não encontrado"),
-    DADOS_INVALIDOS("Dados inválidos"),
-    PARAMETRO_INVALIDO("Parâmetro inválido"),
-    ERRO_DE_SISTEMA("Erro de sistema"),
-    ERRO_NEGOCIO("Violação de regra de negócio"),
-    ENTIDADE_EM_USO("Entidade em uso");
+    RECURSO_NAO_ENCONTRADO("Recurso não encontrado!"),
+    DADOS_INVALIDOS("Dados inválidos!"),
+    PARAMETRO_INVALIDO("Parâmetro inválido!"),
+    ERRO_DE_SISTEMA("Erro de sistema!"),
+    ERRO_NEGOCIO("Violação de regra de negócio!"),
+    ENTIDADE_EM_USO("Entidade em uso!");
 
 
     private String title;

e214710774d5242e00c6a9bbc470a534c472f5b9
william.silva <william@viasoft.com.br>
2021-10-30 10:18:37 -0300

    Criação da classe Cargo

---
+++ src/main/java/com/williamsilva/funcionarioapi/domain/model/Cargo.java
@@ -0,0 +1,39 @@
+package com.williamsilva.funcionarioapi.domain.model;
+
+import java.util.Objects;
+
+public class Cargo {
+
+    private Integer id;
+
+    private String descricao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cargo cargo = (Cargo) o;
+        return Objects.equals(id, cargo.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
